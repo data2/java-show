df8722bf5fa4335da8c6c784006a7d28f15b8d4d
data2 <muskteerathos@gmail.com>
2020-09-01 19:25:07 +0800

    init java-show

---
+++ src/main/java/com/data2/java/show/JavaShowApplication.java
@@ -0,0 +1,13 @@
+package com.data2.java.show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaShowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/data2/java/show/JavaShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data2.java.show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

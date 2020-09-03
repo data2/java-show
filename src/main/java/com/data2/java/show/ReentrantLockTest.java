65c223a55b36cdef69a3961b53bc4b21176bf35f
data2 <muskteerathos@gmail.com>
2020-09-03 22:02:26 +0800

    reentrantlock condition 实现消息队列

--- src/main/java/com/data2/java/show/ReentrantLockTest.java
+++ src/main/java/com/data2/java/show/ReentrantLockTest.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -87,6 +88,72 @@ public class ReentrantLockTest {
 
     @Test
     public void condition() throws InterruptedException {
+        ReentrantLock reentrantLock = new ReentrantLock();
+        Random random = new Random();
+
+        ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+
+        Condition produceCondition = reentrantLock.newCondition();
+        Condition consumerCondition = reentrantLock.newCondition();
+
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(2,5,
+                30,TimeUnit.SECONDS, new ArrayBlockingQueue<>(10), new ThreadPoolExecutor.DiscardPolicy());
+
+        // 生产者
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try{
+                        reentrantLock.lock();
+                        // not full, put
+                        if (queue.size() < 10 ){
+                            try {
+                                Integer val = Integer.valueOf(random.nextInt(10));
+                                queue.put(val);
+                                log.info("生产：{}",val );
+                                // 生产完 signal 消费者
+                                consumerCondition.signalAll();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }else {
+                            produceCondition.await();
+                        }
+                    }catch (Exception e){
+                    }finally {
+                        reentrantLock.unlock();
+                    }
+                }
+            }
+        });
+
+        // 消费者
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                while(true){
+                    try{
+                        reentrantLock.lock();
+                        // not empty take
+                        if (!queue.isEmpty()){
+                            log.info("消费：{}",queue.take());
+                            // 消费完 signal 生产者
+                            produceCondition.signalAll();
+                        }else {
+                            consumerCondition.await();
+                        }
+
+                    }catch (Exception e){
+                    }finally {
+                        reentrantLock.unlock();
+                    }
+                }
+            }
+        });
+
+
+        Thread.sleep(30000);
 
     }
 }

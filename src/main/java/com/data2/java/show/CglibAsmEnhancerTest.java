35ec287f84338d74440c0f9861875bc653b604f2
data2 <muskteerathos@gmail.com>
2020-09-04 21:14:07 +0800

    动态代理

--- src/main/java/com/data2/java/show/CglibAsmEnhancerTest.java
+++ src/main/java/com/data2/java/show/CglibAsmEnhancerTest.java
@@ -1,9 +1,49 @@
 package com.data2.java.show;
 
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.InvocationHandler;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.lang.reflect.Method;
+
 /**
  * @author leewow
  * @description
  * @date 2020/9/4 下午5:48
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
 public class CglibAsmEnhancerTest {
+
+    @Test
+    public void test(){
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(AtomicLongTest.class);
+        enhancer.setClassLoader(getClass().getClassLoader());
+        enhancer.setCallback(new MethodInterceptor() {
+            @Override
+            public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                log.info("调用方法:{}, 参数:{}",method.getName(),objects);
+                return methodProxy.invokeSuper(o,objects);
+            }
+
+
+        });
+        AtomicLongTest test = (AtomicLongTest) enhancer.create();
+        test.test();
+    }
+
+
+
+
+
+
 }
---
+++ src/main/java/com/data2/java/show/JdkProxy.java
@@ -0,0 +1,9 @@
+package com.data2.java.show;
+
+/**
+ * @author leewow
+ * @description
+ * @date 2020/9/4 下午9:13
+ */
+public class JdkProxy {
+}

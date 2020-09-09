1fc0694d3fba98df3373befe08badad125334b1a
data2 <muskteerathos@gmail.com>
2020-09-09 12:53:24 +0800

    ..

---
+++ src/main/java/com/data2/java/show/designpatterns/PrototypePatternTest.java
@@ -0,0 +1,19 @@
+package com.data2.java.show.designpatterns;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author leewow
+ * @description
+ * @date 2020/9/7 下午9:51
+ *
+ * 原型模式：创建现有对象的副本并根据需要进行修改，而不是从头开始创建对象并进行设置
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class PrototypePatternTest {
+}
